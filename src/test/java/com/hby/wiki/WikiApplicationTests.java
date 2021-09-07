a97cf3cfd00cfa8ed96938edf3932da860086dd3
HBY <1534512408@qq.com>
2021-09-07 08:34:15 +0800

    init

---
+++ src/test/java/com/hby/wiki/WikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hby.wiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
